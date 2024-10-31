93b8335606dbb75ae6ce211c8b222613b43d1c6d
horang <fnelclsrn123@gmail,com>
2024-10-31 19:56:44 +0900

    refactor: 스웨거 설정 변경

---
+++ src/main/java/com/plaything/api/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.plaything.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Plaything API DOC",
+                description = "Plaything-app API 설명서입니다."
+        )
+)
+public class SwaggerConfig {
+
+    private final String serverUrl;
+
+    public SwaggerConfig(@Value("${swagger.server.url}") String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Authorization", securityScheme))  // Components 추가
+                .addSecurityItem(securityRequirement);
+    }
+    }
