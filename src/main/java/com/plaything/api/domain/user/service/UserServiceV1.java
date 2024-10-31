1f711fc6e7c93b85e6671265d936721d4e150ec6
horang <fnelclsrn123@gmail,com>
2024-10-31 19:55:09 +0900

    feat: 유저 이름으로 찾는 기능

--- src/main/java/com/plaything/api/domain/user/service/UserServiceV1.java
+++ src/main/java/com/plaything/api/domain/user/service/UserServiceV1.java
@@ -1,7 +1,9 @@
 package com.plaything.api.domain.user.service;
 
+import com.plaything.api.common.exception.CustomException;
 import com.plaything.api.common.exception.ErrorCode;
-import com.plaything.api.domain.repository.UserRepository;
+import com.plaything.api.domain.repository.user.UserRepository;
+import com.plaything.api.domain.repository.entity.user.User;
 import com.plaything.api.domain.user.model.response.UserSearchResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,11 +20,11 @@ public class UserServiceV1 {
 
     public UserSearchResponse searchUser(String name, String user){
         List<String> nameByNameMatch = userRepository.findNameByNameMatch(name, user);
-
         return new UserSearchResponse(ErrorCode.SUCCESS, nameByNameMatch);
-
     }
 
-
-
+    public User findByName(String name){
+       return userRepository.findByName(name)
+               .orElseThrow(()->new CustomException(ErrorCode.NOT_EXIST_USER));
+    }
 }
