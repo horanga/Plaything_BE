623de4c366606ac257b2d28dd520003ee2e8e8e7
horang <fnelclsrn123@gmail.com>
2024-11-06 17:27:13 +0900

    refactor: 반환타입 변경

--- src/main/java/com/plaything/api/domain/user/service/ProfileImageServiceV1.java
+++ src/main/java/com/plaything/api/domain/user/service/ProfileImageServiceV1.java
@@ -5,14 +5,14 @@ import com.plaything.api.common.exception.ErrorCode;
 import com.plaything.api.domain.image.service.model.SavedImage;
 import com.plaything.api.domain.repository.entity.user.ProfileImage;
 import com.plaything.api.domain.repository.entity.user.User;
-import com.plaything.api.domain.repository.user.ProfileImageRepository;
+import com.plaything.api.domain.repository.entity.user.profile.Profile;
+import com.plaything.api.domain.repository.repo.user.ProfileImageRepository;
 import com.plaything.api.domain.user.model.response.ProfileImageResponse;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.*;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,28 +22,24 @@ public class ProfileImageServiceV1 {
     private final ProfileImagesRegistrationServiceV1 profileImagesRegistrationServiceV1;
 
     @Transactional
-    public void saveImage(SavedImage image, User user) {
+    public ProfileImage saveImage(SavedImage image, Profile profile) {
 
         ProfileImage imageToSave = ProfileImage.builder()
                 .url(image.url())
                 .fileName(image.fileName())
-                .user(user)
+                .profile(profile)
                 .build();
         try {
-            profileImageRepository.save(imageToSave);
+            ProfileImage profileImage = profileImageRepository.save(imageToSave);
             profileImagesRegistrationServiceV1.saveImageRegistration(imageToSave);
+            return profileImage;
 
         } catch (Exception e) {
             throw new CustomException(ErrorCode.IMAGE_SAVED_FAILED);
         }
     }
 
-    public List<ProfileImageResponse> getProfileImages(User user){
-        return profileImageRepository.findByUser(user)
-                .stream().map(ProfileImageResponse::toResponse).toList();
-    }
-
-    public long checkCountOfImages(String user){
-       return profileImageRepository.countByUser_name(user);
+    public long checkCountOfImages(Long profileId){
+       return profileImageRepository.countByProfile_Id(profileId);
     }
 }
