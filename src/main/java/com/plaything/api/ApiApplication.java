f090b75a491220a4ac40a5e7737810d1742e48c0
horang <fnelclsrn123@gmail.com>
2024-11-22 03:04:40 +0900

    feat: 서킷 브레이커 관련 로그 추가

--- src/main/java/com/plaything/api/ApiApplication.java
+++ src/main/java/com/plaything/api/ApiApplication.java
@@ -1,14 +1,52 @@
 package com.plaything.api;
 
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+@Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> myRegistryEventConsumer() {
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry().getEventPublisher()
+                        .onStateTransition(event -> log.warn("CircuitBreaker {} state changed from {} to {}",
+                                event.getCircuitBreakerName(),
+                                event.getStateTransition().getFromState(),
+                                event.getStateTransition().getToState()))
+                        .onFailureRateExceeded(event -> log.error("CircuitBreaker {} failure rate exceeded: {}",
+                                event.getCircuitBreakerName(),
+                                event.getFailureRate()));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+            }
+        };
+    }
 }
+
