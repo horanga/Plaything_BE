fb376e73759c98085225a50f269816693952fdd2
horang <fnelclsrn123@gmail.com>
2024-11-22 02:50:32 +0900

    feat: 광고 시청 로그 생성

---
+++ src/main/java/com/plaything/api/domain/key/service/AdLogServiceV1.java
@@ -0,0 +1,34 @@
+package com.plaything.api.domain.key.service;
+
+import com.plaything.api.domain.key.model.request.AdRewardRequest;
+import com.plaything.api.domain.key.model.response.AdViewLogResponse;
+import com.plaything.api.domain.repository.entity.log.AdViewLog;
+import com.plaything.api.domain.repository.entity.user.User;
+import com.plaything.api.domain.repository.repo.log.AdViewLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class AdLogServiceV1 {
+
+    private final AdViewLogRepository adViewLogRepository;
+
+    public void createAdViewLog(User user, AdRewardRequest request) throws IOException {
+        AdViewLog adViewLog = AdViewLog.builder()
+                .viewDuration(request.viewDuration())
+                .adType(request.adType())
+                .user(user)
+                .build();
+        adViewLogRepository.save(adViewLog);
+    }
+
+    public List<AdViewLogResponse> getAdViewLog(String loginId) {
+        return adViewLogRepository.findByUser_LoginId(loginId).stream()
+                .map(AdViewLogResponse::toResponse)
+                .toList();
+    }
+}
