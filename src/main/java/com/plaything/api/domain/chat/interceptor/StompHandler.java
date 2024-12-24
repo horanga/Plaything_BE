2f217bc53b85b47521236fdde2243722c72a5636
fnelc <fnelclsrn123@gmail.com>
2024-12-24 15:59:47 +0900

    refactor: 프로덕션 코드 변경에 따른 테스트 변경

--- src/main/java/com/plaything/api/domain/chat/interceptor/StompHandler.java
+++ src/main/java/com/plaything/api/domain/chat/interceptor/StompHandler.java
@@ -1,5 +1,9 @@
 package com.plaything.api.domain.chat.interceptor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plaything.api.domain.chat.model.reqeust.ChatRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -17,6 +21,8 @@ public class StompHandler implements ChannelInterceptor {
 
     private final SessionValidator sessionValidator;
 
+    private final ObjectMapper objectMapper;
+
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -29,7 +35,7 @@ public class StompHandler implements ChannelInterceptor {
         String destination = accessor.getDestination();
 
         if (command != null && command.equals(StompCommand.SEND)) {
-            sessionValidator.validateSend(authHeader, sessionId, destination, message);
+            sessionValidator.validateSend(authHeader, sessionId, destination, parseMessage(message));
             return message;
         }
 
@@ -49,6 +55,28 @@ public class StompHandler implements ChannelInterceptor {
         }
 
         return message;
+    }
+
+    private ChatRequest parseMessage(Message<?> message) {
+        ChatRequest chatRequest = null;
+
+        try {
+            // payload가 byte[] 형태로 들어있을 수 있음
+            Object payload = message.getPayload();
+            if (payload instanceof byte[]) {
+                String jsonString = new String((byte[]) payload);
+                chatRequest = objectMapper.readValue(jsonString, ChatRequest.class);
+            }
+            // 또는 이미 String 형태일 수 있음
+            else if (payload instanceof String) {
+                chatRequest = objectMapper.readValue((String) payload, ChatRequest.class);
+            }
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return chatRequest;
     }
 
 }
