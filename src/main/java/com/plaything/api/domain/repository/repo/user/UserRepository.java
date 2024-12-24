73245b6cd77503207d66b8316fde4aed1eff8038
fnelc <fnelclsrn123@gmail.com>
2024-12-24 19:59:43 +0900

    refactor: ci/cd 용 커밋

--- src/main/java/com/plaything/api/domain/repository/repo/user/UserRepository.java
+++ src/main/java/com/plaything/api/domain/repository/repo/user/UserRepository.java
@@ -13,7 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByLoginId(String loginId);
 
-    //pattern, user를 기준으로 네임값을 가져온다. locate -->데이터에 있는 name값이 패턴에 몇번째(인덱스)에 위치하는지 -->0보다 크면 패턴 만족
     @Query("Select u.loginId from User u where LOCATE(LOWER(:pattern), lower(u.loginId)) >0 and u.loginId !=:user")
     List<String> findLoginIdByLoginIdMatch(@Param("pattern") String pattern, @Param("user") String user);
 
