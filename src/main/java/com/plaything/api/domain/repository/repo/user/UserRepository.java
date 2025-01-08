9da0e307e9096dee47b1ea36100fe61a17816204
horanga <fnelclsrn123@gmail.com>
2025-01-08 22:22:27 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/com/plaything/api/domain/repository/repo/user/UserRepository.java
+++ src/main/java/com/plaything/api/domain/repository/repo/user/UserRepository.java
@@ -1,21 +1,11 @@
 package com.plaything.api.domain.repository.repo.user;
 
 import com.plaything.api.domain.repository.entity.user.User;
-import com.plaything.api.domain.profile.constants.PersonalityTraitConstant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByLoginId(String loginId);
-
-    @Query("Select u.loginId from User u where LOCATE(LOWER(:pattern), lower(u.loginId)) >0 and u.loginId !=:user")
-    List<String> findLoginIdByLoginIdMatch(@Param("pattern") String pattern, @Param("user") String user);
-
-    List<User> findUserByProfile_PersonalityTrait_trait(PersonalityTraitConstant trait);
-
 }
