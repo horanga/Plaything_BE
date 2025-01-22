f527416518af436e56516018b3956f37d96153d4
horanga <fnelclsrn123@gmail.com>
2025-01-22 22:58:32 +0900

    chore: 구글 로그인 요청 dto 이름 변경

--- src/main/java/com/plaything/api/domain/auth/client/dto/request/GoogleLoginRequest.java
+++ src/main/java/com/plaything/api/domain/auth/client/dto/request/GoogleLoginRequest.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Login 요청")
-public record LoginRequest(
-        @Schema(description = "OAuth2 액세스 토큰")
+public record GoogleLoginRequest(
+        @Schema(description = "구글 OAuth2 액세스 토큰")
         @NotBlank
         @NotNull
         String accessToken,
